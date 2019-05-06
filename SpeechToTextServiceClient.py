450dbbfd20cc259be5de32e53c4107aecbe845bc
Abhishek <abhishek@DN51us1f.SUNet>
2019-05-05 17:27:31 -0700

    speech api formatting fixed

--- SpeechToTextServiceClient.py
+++ SpeechToTextServiceClient.py
@@ -8,18 +8,27 @@ from google.cloud.speech import types
 class SpeechToTextServiceClient:
 
     def __init__(self):
-        #Environment variable
+        #Environment variable GOOGLE_APPLICATION_CREDENTIALS needs to
+        # be set to googleauth-adde97af7066
         self.client = speech.SpeechClient()
 
-    def transcribeAudioFile(self, file_name=None):
+    def transcribeAudioFile(self, file_name, file_format='wav'):
         # Loads the audio into memory
         info = mediainfo(file_name)
         with open(file_name, 'rb') as audio_file:
             content = audio_file.read()
             audio = types.RecognitionAudio(content=content)
 
+        if file_format == 'flac':
+            encoding = enums.RecognitionConfig.AudioEncoding.FLAC
+        elif file_format == 'wav':
+            encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+        else:
+            print("Error: only .flac and .wav formats are supported.")
+            return
+
         config = types.RecognitionConfig(
-            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            encoding=encoding,
             sample_rate_hertz=int(float(info['sample_rate'])),
             audio_channel_count=int(info['channels']),
             use_enhanced=True,
@@ -28,20 +37,18 @@ class SpeechToTextServiceClient:
 
         # Detects speech in the audio file
         response = self.client.recognize(config, audio)
-        print(response)
 
+        transcript = 'Transcript: '
         for result in response.results:
-            print('Transcript: {}'.format(result.alternatives[0].transcript))
-
-        return response.results
+            transcript += result.alternatives[0].transcript
+        print(transcript)
+        return transcript
 
 if __name__=="__main__":
     client = SpeechToTextServiceClient()
     filename = os.path.join(
                 os.path.dirname(__file__),
                 'recordings',
-                'test3.wav')
+                'test.wav')
 
-    # a = AudioSegment.from_wav(filename)
-    # a.export("./recordings/test4.flac", format="flac")
     client.transcribeAudioFile(filename)
